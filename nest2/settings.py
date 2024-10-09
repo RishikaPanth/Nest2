032ec987fb51690011a8c2b94188bdb720fe9abf
rishabh132003 <103423564+rishabh132003@users.noreply.github.com>
2024-10-10 04:10:47 +0530

    a

--- nest2/settings.py
+++ nest2/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'your_app.middleware.NoCacheMiddleware',
+    'nestapp.middleware.NoCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'nest2.urls'
